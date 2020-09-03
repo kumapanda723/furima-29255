70c75afa90af1e4d38f823a971abe2504f26af6c
kumapanda723 <kumapanda723@icloud.com>
2020-09-04 01:15:34 +0900

    トップページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'posts/index'
-  root to: 'posts#index'
+  root to: 'items#index'
+  #resources :users, only: [:]
+  #resources :items, only: :index
+  #resources :orders, only: [:index, :create]
 end
