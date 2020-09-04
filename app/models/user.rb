55f51b701fcb5f3a85cef5721f29c22b96ad04fd
kumapanda723 <kumapanda723@icloud.com>
2020-09-04 17:24:18 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,31 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname,
+    presence: true
+
+  validates :email,
+    presence: true,
+    uniqueness: true
+
+  validates :password,
+    presence: true,
+    length: { minimum: 6 },
+    confirmation: true,
+    format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}\z/ }
+
+  validates :birthday,
+      presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+     validates :first_name
+     validates :family_name
+  end
+
+  with_options presence: true, format: { with: /\A([ァ-ン]|ー)+\z/ } do
+     validates :first_name_kana
+     validates :family_name_kana
+  end
+
 end
