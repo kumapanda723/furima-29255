1136f5346c3945bde03afa6afc064ff524fe5ba4
kumapanda723 <kumapanda723@icloud.com>
2020-09-06 16:06:19 +0900

    usersモデルの単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,19 +15,22 @@ class User < ApplicationRecord
     presence: true,
     length: { minimum: 6 },
     confirmation: true,
-    format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}\z/ }
+    format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: "Include both letters and numbers" }
 
   validates :birthday,
       presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
-     validates :first_name
-     validates :family_name
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" } do
+    validates :first_name
+    validates :family_name
   end
 
-  with_options presence: true, format: { with: /\A([ァ-ン]|ー)+\z/ } do
-     validates :first_name_kana
-     validates :family_name_kana
+  with_options presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, message: "Full-width katakana characters" } do
+    validates :first_name_kana
+    validates :family_name_kana
   end
+
+  has_many :items
+  has_many :orders
 
 end
