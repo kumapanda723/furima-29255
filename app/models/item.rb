9cf459b2b791ad270a598ea44a161349ec25671c
kumapanda723 <kumapanda723@icloud.com>
2020-09-07 19:39:31 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,26 @@
-class Item < ApplicationRecordend
+class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :name
+    validates :detail
+    validates :user
+    validates :category_id
+    validates :status_id
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' },
+                      numericality: { only_integer: true, greater_than: 299, less_than: 10000000 }
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
+
   belongs_to :user
   has_one :order
-  belongs_to_active_hash :category
-  belongs_to_active_hash :status
-  belongs_to_active_hash :delivery_fee
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :shipping_date
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :category
+    belongs_to_active_hash :status
+    belongs_to_active_hash :delivery_fee
+    belongs_to_active_hash :prefecture
+    belongs_to_active_hash :shipping_date
+  
 end
