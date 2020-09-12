45d9c5a7d0039a2ea219606aefd335205345d52e
kumapanda723 <kumapanda723@icloud.com>
2020-09-12 11:26:45 +0900

    ordersテーブルの作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,34 @@
+class OrdersController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save1
+      return redirect_to root_path
+    end
+    render 'index'
+  end
+
+  private
+
+  def order_params
+    params.permit(:token, :user, :item)
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency:'jpy'
+    )
+  end
+
+end
---
+++ app/models/order.rb
@@ -0,0 +1,11 @@
+class Order < ApplicationRecord
+  
+  with_options presence: true do
+     validates :user
+     validates :item
+  end
+
+    belongs_to :user
+    belongs_to :item
+    has_one :address
+end
