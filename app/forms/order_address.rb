22722c6d94bb68b9c34756a9f374b69f005305be
kumapanda723 <kumapanda723@icloud.com>
2020-09-15 18:11:21 +0900

    バリデーションの修正

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -4,16 +4,15 @@ class OrderAddress
   attr_accessor :name, :user_id, :item_id, :postal_code, :prefecture_id, :city, :address_line, :building, :phone, :authenticity_token, :token, :order_id
   
   with_options presence: true do
-     validates :phone
+     validates :phone, format: { with: /\A\d{,11}\z/ }
      validates :address_line
      validates :city
-     validates :postal_code
-     validates :prefecture_id
-     validates :token
+     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
+     validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
    end
 
   def save
     order = Order.create(user_id:user_id, item_id:item_id)
-    Address.create(phone:phone, address_line:address_line, city:city, postal_code:postal_code, order_id:order.id, building:building)
+    Address.create(prefecure_id:prefecture_id, phone:phone, address_line:address_line, city:city, postal_code:postal_code, order_id:order.id, building:building)
   end
 end
